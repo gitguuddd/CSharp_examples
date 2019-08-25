4b5dbd3eb7b36f7af5655ad0673bb2b5099f8efe
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-08-26 00:25:41 +0300

    A simple example of dynamic variable usage

---
+++ Dynamic_var/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic dynamicVariable = 100;
+            Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+
+            dynamicVariable = "Hello World!!";
+            Console.WriteLine("Dynamic variable values: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+
+            dynamicVariable = true;
+            Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+
+            dynamicVariable = DateTime.Now;
+            Console.WriteLine("Dynamic variable value: {0}, Type: {1}", dynamicVariable, dynamicVariable.GetType().ToString());
+            Console.ReadLine();
+        }
+    }
+}

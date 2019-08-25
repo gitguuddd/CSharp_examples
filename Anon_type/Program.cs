0e692f4900972af494976ea26e6a84fed3eb8beb
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-08-26 00:11:10 +0300

    Passing an anonymous type to a method through a dynamic parameter

---
+++ Anon_type/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anon_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAnonymousType = new
+            {
+                firstProperty = "First Property",
+                secondProperty = 2,
+                thirdProperty = true
+            };
+            DoSomething(myAnonymousType);
+            Console.ReadLine();
+        }
+
+        static void DoSomething(dynamic param)
+        {
+            Console.WriteLine(param.firstProperty);
+        }
+    }
+}

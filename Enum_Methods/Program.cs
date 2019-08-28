46ebc83d34dad0d82d31c295391cfd8e0934a5f0
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-08-28 19:52:32 +0300

    A simple example of a few methods belonging to enumerator on c#

---
+++ Enum_Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Methods
+{
+    class Program
+    {
+        enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Enum.GetName(typeof(WeekDays), 4));
+
+            Console.WriteLine("WeekDays constant names:");
+
+            foreach (string str in Enum.GetNames(typeof(WeekDays)))
+                Console.WriteLine(str);
+
+            Console.WriteLine("Enum.TryParse():");
+
+            WeekDays wdEnum;
+            Enum.TryParse<WeekDays>("1", out wdEnum);
+            Console.WriteLine(wdEnum);
+            Console.ReadLine();
+        }
+    }
+}

da946123266364d8c6bd0cc2e55d1e1db137cb92
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-08-30 23:50:09 +0300

    An example of some simple uses for static modifier in C#

---
+++ Static/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    class Program
+    {
+        public class MyNormalClass
+        {
+            public static int ID = 0;
+            public static int MethodCalled = 0;
+            static MyNormalClass()
+            {
+                Console.WriteLine("First object instantiated");
+            }
+            public MyNormalClass()
+            {
+                Console.WriteLine("{0} object/s of MyNormalClass created so far", ++ID);
+            }
+            public static void MyStaticMethod()
+            {
+                Console.WriteLine("This is a static method which has been called {0} times",++MethodCalled);
+            }
+        }
+        
+        public static class MyStaticClass
+        {
+            public static int MyStaticProperty { get; set; }
+            static MyStaticClass()
+            {
+                Console.WriteLine("Static class accessed for the first time");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var test1 = new MyNormalClass();
+            var test2 = new MyNormalClass();
+            var test3 = new MyNormalClass();
+            MyNormalClass.MyStaticMethod();
+            MyNormalClass.MyStaticMethod();
+            MyNormalClass.MyStaticMethod();
+            MyStaticClass.MyStaticProperty = 100;
+            MyStaticClass.MyStaticProperty = 200;
+            Console.WriteLine(MyStaticClass.MyStaticProperty);
+            Console.ReadLine();
+        }
+    }
+}

854186fba28eb8c4e8ac09c6e4b2d625a07ebf5a
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-09-01 02:55:06 +0300

    A simple example of how to use an indexer with a custom class

---
+++ Indexer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class StringDataStore
+    {
+        private string[] strArr = new string[10];
+        public StringDataStore()
+        {
+
+        }
+        public string this[int index]
+        {
+            get { if (index < 0 && index >= strArr.Length)
+                    throw new IndexOutOfRangeException("Cannot store more than 10 objects");
+
+                return strArr[index]; }
+            set { if (index < 0 && index >= strArr.Length)
+                    throw new IndexOutOfRangeException("Cannot store more than 10 objects");
+               strArr[index] = value;
+            }
+        }
+
+        public string this[string name]
+        {
+            get
+            {
+                foreach (string str in strArr)
+                {
+                    if (str.ToLower() == name.ToLower())
+                        return str;
+                }
+                return null;
+            }
+
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringDataStore strStore = new StringDataStore();
+            strStore[0] = "One";
+            strStore[1] = "Two";
+            strStore[2] = "Three";
+            strStore[3] = "Four";
+
+            Console.WriteLine(strStore[0]);
+            Console.WriteLine(strStore[1]);
+            Console.WriteLine(strStore["Three"]);
+            Console.WriteLine(strStore["Four"]);
+        }
+    }
+}

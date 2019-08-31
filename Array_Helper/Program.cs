b7a3d3699671056daf8a82e78d280b012e2046b3
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-09-01 00:54:18 +0300

    A simple example how to use array helper to start indexing for 1

---
+++ Array_Helper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array array = Array.CreateInstance(typeof(int), new int[1] { 5 }, new int[1] { 1 });
+            array.SetValue(1, 1);
+            array.SetValue(2, 2);
+            array.SetValue(3, 3);
+            array.SetValue(4, 4);
+            array.SetValue(5, 5);
+            foreach (int i in array)
+                Console.WriteLine("Array value {0} at position {1}", array.GetValue(i), i);
+
+        }
+    }
+}

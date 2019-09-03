b4cc0c56bf84e056f4668e208afb57f7523580e2
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-09-04 00:09:50 +0300

    A simple example of some of the functions of Stack collection

---
+++ Stack_/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Stack_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+            myStack.Push("Hello!");
+            myStack.Push(null);
+            myStack.Push(1);
+            myStack.Push(2);
+            myStack.Push(3);
+
+            foreach (var itm in myStack)
+                Console.WriteLine(itm);
+
+            Console.WriteLine($" The current top element is :{myStack.Peek()}");
+
+            Console.WriteLine($"Number of elements in the stack: {myStack.Count}");
+
+            myStack.Pop();
+            myStack.Pop();
+            myStack.Pop();
+
+            Console.WriteLine($"Number of elements in the stack: {myStack.Count}");
+
+            Console.WriteLine($"Does stack contain a null element ? :{myStack.Contains(null)}");
+
+            myStack.Clear();
+
+            Console.WriteLine($"Number of elements in the stack: {myStack.Count}");
+        }
+    }
+}

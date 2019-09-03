273ba102bf61d181b3ed2113f4df0f8c6415beaf
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-09-04 00:44:02 +0300

    A simple example how to use generics

---
+++ Generics/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class MyGenericClass<T>
+    {
+        private T genericMemberVariable;
+
+        public MyGenericClass()
+        {
+            
+        }
+        public MyGenericClass(T value)
+        {
+            genericMemberVariable = value;
+        }
+
+        public T genericMethod(T genericParameter)
+        {
+            Console.WriteLine($"Parameter type: {typeof(T).ToString()}, value: {genericParameter}");
+            Console.WriteLine($"Return type: {typeof(T).ToString()}, value: {genericMemberVariable}");
+
+            return genericMemberVariable;
+        }
+
+        public T genericProperty { get; set; }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        public delegate T add<T>(T param1, T param2);
+        static void Main(string[] args)
+        {
+            MyGenericClass<int> intGenericClass = new MyGenericClass<int>(10);
+
+            int val = intGenericClass.genericMethod(200);
+
+            add<int> sum = AddNumber;
+
+            Console.WriteLine(sum(10, 20));
+
+            add<string> conct = Concate;
+
+            Console.WriteLine(conct("Hello", "World!!"));
+        }
+
+        public static int AddNumber(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+
+        public static string Concate(string str1, string str2)
+        {
+            return str1 + str2;
+        }
+    }
+}

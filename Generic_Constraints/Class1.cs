0fd5ba4d8e7aa51d2f0569a2257b7934cbff7740
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-09-04 01:01:12 +0300

    A simple example of how to use generics constraints

---
+++ Generic_Constraints/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Constraints
+{
+    class MyGenericClass<T, U> where T : class where U : struct
+    {
+        private T genericMemberVariable;
+
+        public MyGenericClass(T value)
+        {
+            genericMemberVariable = value;
+        }
+
+        public T genericMethod<Z>(T genericParameter, Z anotherGenericParameter) where Z : struct
+        {
+            Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), genericParameter);
+            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), genericMemberVariable);
+
+            return genericMemberVariable;
+        }
+
+        public T genericProperty { get; set; }
+    }
+}
---
+++ Generic_Constraints/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Constraints
+{
+    class Program
+    {   /*
+        where T : class	Type must be reference type.
+        where T: struct	Type must be value type.
+        where T: new()	Type must have public parameterless constructor.
+        where T: <base class name>	Type must be or derive from the specified base class
+        where T: <interface name>	Type must be or implement the specified interface.
+        where T: U	Type supplied for T must be or derive from the argument supplied for U.
+        */
+        static void Main(string[] args)
+        {
+            MyGenericClass<string,int> strGenericClass = new MyGenericClass<string,int>("Hello World");
+            string str = "Wow";
+            int a = 10;
+            strGenericClass.genericMethod<int>(str, a);
+            //MyGenericClass<int> intGenericClass = new MyGenericClass<int>;  - Compile time error because of the constraints
+        }
+    }
+}

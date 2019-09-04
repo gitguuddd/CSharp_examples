9379b7183a135e74c2429026b814518ea96b1b8e
Mindaugas Kasiulis <mkasiulis49@gmail.com>
2019-09-04 16:15:13 +0300

    A simple example of some of the functions of List<>

---
+++ List_/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace List_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+            intList.Add(10);
+            intList.Add(20);
+            intList.Add(30);
+            intList.Add(40);
+
+            intList.ForEach(el => Console.WriteLine(el));
+
+            Console.WriteLine($"Total elements: {intList.Count}");
+
+            intList.Insert(1, 11);
+
+            intList.Remove(10);
+            intList.RemoveAt(2);
+
+            Console.WriteLine("List elements after insertion and removal");
+
+            foreach (var el in intList)
+                Console.WriteLine(el);
+
+            Console.WriteLine($"Does list contain only even numbers ?: {intList.TrueForAll(el => el % 2 == 0)}");
+        }
+    }
+}
